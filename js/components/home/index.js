081a2ade767e1e91a593122a0c8d34f60c2fbb2b
Liam Crewe <liam.j.crewe@gmail.com>
2017-01-25 16:43:33 +0000

    Moved history into redux rather than state

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -8,6 +8,7 @@ import { Grid, Row, Col } from 'react-native-easy-grid';
 import { reverse } from 'ramda'
 
 import { setIndex } from '../../actions/list';
+import { setHistory } from '../../actions/wellbeing';
 import myTheme from '../../themes/base-theme';
 import styles from './styles';
 
@@ -35,8 +36,7 @@ class Home extends Component {
     super(props);
     this.state = {
       error: false,
-      message: '',
-      wellbeings: []
+      message: ''
     };
   }
 
@@ -76,9 +76,7 @@ class Home extends Component {
 
         response.json()
           .then(json => {
-            this.setState({
-              wellbeings: reverse(json.results)
-            })
+            this.props.setHistory(reverse(json.results));
           })
       })
       .catch(error => {
@@ -123,7 +121,7 @@ class Home extends Component {
               <Text style={styles.tableHeaderText}>Wellbeing</Text>
             </Col>
           </Row>
-          {this.state.wellbeings.map((wellbeing, i) =>
+          {this.props.history.map((wellbeing, i) =>
             <Row key={i} style={styles.row}>
               <Col style={styles.leftCol} size={3}><Text style={styles.text}>{this.formatDate(wellbeing.date_recorded)}</Text></Col>
               <Col style={styles.rightCol} size={1}><Text style={styles.text}>{wellbeing.wellbeing}</Text></Col>
@@ -147,7 +145,27 @@ function bindAction(dispatch) {
     setIndex: index => dispatch(setIndex(index)),
     pushRoute: (route, key) => dispatch(pushRoute(route, key)),
     reset: key => dispatch(reset([{ key: 'login' }], key, 0)),
+    setHistory: history => dispatch(setHistory(history)),
   };
+}
+
+const formatDate = timestampString => {
+  const date = new Date(timestampString);
+
+  return date.toLocaleDateString('en-GB') + ' at ' + date.toLocaleTimeString();
+}
+
+const getHistoryTableRows = history => {
+  historyTableRows = []
+
+  for (i = 0; i < 5; i++) {
+    historyTableRows[i] = {
+      date_recorded: history[i] ? formatDate(history[i].date_recorded) : '',
+      wellbeing: history[i] ? history[i].wellbeing : '',
+    }
+  }
+
+  return historyTableRows
 }
 
 const mapStateToProps = state => ({
@@ -155,6 +173,7 @@ const mapStateToProps = state => ({
   token: state.user.token,
   list: state.list.list,
   navigation: state.cardNavigation,
+  history: getHistoryTableRows(state.wellbeing.history)
 });
 
 export default connect(mapStateToProps, bindAction)(Home);
